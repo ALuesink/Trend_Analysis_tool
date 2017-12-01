08b450a75a1f6979d1390015c2ab328faa2f3191
aluesink <anneluesink@live.nl>
2017-12-01 14:51:26 +0100

    Changed due to structural changes

--- trend_analysis.py
+++ trend_analysis.py
@@ -2,8 +2,6 @@
 
 import argparse
 
-#import raw_data
-#import processed_data
 import upload
 import delete
 
@@ -18,35 +16,11 @@ def upload_processed_data(args):
 def delete_run_all_data(args):
     delete.run_all.del_all_rundata(args.run)
 
-def delete_run_rawdata(args):
+def delete_run_raw_data(args):
     delete.run_rawdata.del_run_rawdata(args.run)
     
 def delete_sample_data(args):
     delete.sample_processed.del_sampledata(args.run, args.sample)
-
-
-## Upload functions
-#def upload_raw_data(args):
-#    """Upload Raw data to the database"""
-#    raw_data.upload.up_to_database(args.run, args.path, args.sequencer)
-#
-#def upload_processed_data(args):
-#    """Upload Processed data to the database"""
-#    processed_data.upload.up_to_database(args.run, args.path)
-#    
-## Delete functions
-#def del_run_data(args):
-#    """Delete all run data from the database"""
-#    raw_data.delete.del_from_database(args.run)
-#
-#def del_processed_data(args):
-#    """Delete processed run data from the database"""
-#    processed_data.delete.del_from_database(args.run)
-#
-## Update functions
-#def update_run_data(args):
-#    """Update the Run data by first delete all data and then upload the new data"""
-#    raw_data.update.up_Run_in_database(args.run, args.path, args.sequencer)
 
 
 if __name__ == "__main__":
@@ -63,16 +37,20 @@ if __name__ == "__main__":
     parser_raw_upload.add_argument('run', help='Run name')
     parser_raw_upload.add_argument('path', help='Path to run')
     parser_raw_upload.add_argument('sequencer', help='Sequencer name')
-#    parser_raw_upload.set_defaults(func=upload_raw_data)
+    parser_raw_upload.set_defaults(func=upload_raw_data)
     
     parser_raw_delete = subparser_raw.add_parser('delete', help='delete all run data from database')
     parser_raw_delete.add_argument('run', help='Run name')
-#    parser_raw_delete.set_defaults(func=del_run_data)
+    parser_raw_delete.set_defaults(func=delete_run_all_data)
+    
+    parser_raw_delete = subparser_raw.add_parser('delete', help='delete all raw run data from database')
+    parser_raw_delete.add_argument('run', help='Run name')
+    parser_raw_delete.set_defaults(func=delete_run_raw_data)
 
-    parser_raw_update = subparser_raw.add_parser('update', help='delete all run data and upload new run data to database')    
-    parser_raw_update.add_argument('run', help='Run name')
-    parser_raw_update.add_argument('path', help='Path to run')
-    parser_raw_update.add_argument('sequencer', help='Sequencer name')
+#    parser_raw_update = subparser_raw.add_parser('update', help='delete all run data and upload new run data to database')    
+#    parser_raw_update.add_argument('run', help='Run name')
+#    parser_raw_update.add_argument('path', help='Path to run')
+#    parser_raw_update.add_argument('sequencer', help='Sequencer name')
 #    parser_raw_update.set_defaults(func=update_run_data)
     
     # processed data
@@ -82,11 +60,11 @@ if __name__ == "__main__":
     parser_processed_upload = subparser_processed.add_parser('upload', help='upload processed data to database')
     parser_processed_upload.add_argument('run', help='Run name')
     parser_processed_upload.add_argument('path', help='Path to run')
-#    parser_processed_upload.set_defaults(func=upload_processed_data)
+    parser_processed_upload.set_defaults(func=upload_processed_data)
     
-    parser_processed_delete = subparser_processed.add_parser('delete', help='delete processed run data from database')
+    parser_processed_delete = subparser_processed.add_parser('delete', help='delete processed sample data from database')
     parser_processed_delete.add_argument('run', help='Run name')
-#    parser_processed_delete.set_defaults(func=del_processed_data)
+    parser_processed_delete.set_defaults(func=delete_sample_data)
     
     args = parser.parse_args()
     args.func(args) 
