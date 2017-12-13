94624a044d693c4fa0a8dbc5417f5c8c4ce4d39a
aluesink <anneluesink@live.nl>
2017-12-13 14:22:09 +0100

    Added sequencers to config file

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 MySQL_DB = {
-	"username" : "####",
-	"password" : "$$$$$$$$$$$$$$$$$",
-	"host" : "%%%%%%%%%%%%%%%%%%",
-	"database" : "^^^^^" }
+	"username" : "@@@@@@",
+	"password" : "################",
+	"host" : "$$$$$$$$$$$$$$$$$$",
+	"database" : "%%%%%%%%%%%%%5" }
+
+Sequencers = ["hiseq_umc01", "nextseq_umc01", "nextseq_umc02", "novaseq_umc01"]
