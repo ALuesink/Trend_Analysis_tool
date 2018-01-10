e0782a274cb73af88c64c08276b988f5124eafe8
aluesink <anneluesink@live.nl>
2018-01-10 09:46:05 +0100

    Added NICU to set run name

--- scripts/database/set_run.py
+++ scripts/database/set_run.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 """Set run name"""
 
+
 def set_run_name(run):
     if run.count("_") == 4:
         index = run.rfind("_")
         run = run[:index]
-        
+    elif run.count("_") == 5:
+        index1 = run.rfind("_")
+        index2 = run.rfind("_", 0, index1)
+        run = run[:index2]
+
     return run
